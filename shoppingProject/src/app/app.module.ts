ab88079381acd3cba594d4c8efcc0adae860bfda
Ronald Stanley <rstanley@azgfd.gov>
2019-05-01 17:33:26 -0700

    Current thru section 221, 5/1/19

--- shoppingProject/src/app/app.module.ts
+++ shoppingProject/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -34,6 +34,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService],
--- shoppingProject/src/app/recipes/recipe.service.ts
+++ shoppingProject/src/app/recipes/recipe.service.ts
@@ -1,24 +1,24 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter, Injectable } from '@angular/core';
+import {  Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
+    
 
     private recipes: Recipe[] = [
         new Recipe(
-        'A Test Recipe', 
-        'This is a test description', 
+        'Chicken Recipe', 
+        'Chicken and Lime', 
         'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*',
         [
             new Ingredient('Chicken', 4),
             new Ingredient('Lime', 3)
         ]),
         new Recipe(
-            'A Second Recipe',
-            'A second is a test description',
+            'Cheeseburger Recipe',
+            'A tasty hot cheeseburger',
             'https://s3.amazonaws.com/pixtruder/original_images/9fbb73a7cba9b7b75c3d05484f6b6d087470641d',
             [
                 new Ingredient('Buns', 2),
